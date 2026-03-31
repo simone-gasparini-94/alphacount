267c55c990005a2beff8e83fe06ae607afce2519
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-03-31 15:36:28 +0200

    Print books number of words

--- main.py
+++ main.py
@@ -3,9 +3,14 @@ def get_book_text(path):
         return file.read()
 
 
+def get_num_of_words(string):
+    return len(string.split())
+
+
 def main():
     frankenstein_path = "./books/frankenstein.txt"
-    print(get_book_text(frankenstein_path))
+    num_words = get_num_of_words(get_book_text(frankenstein_path))
+    print(f"Found {num_words} total words")
 
 
 main()
