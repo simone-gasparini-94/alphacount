d95a8eeaf6341d88a37dac61591bf40dbfb4a3d4
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-03-31 15:51:54 +0200

    Add function to count how many times a character appears

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
-from stats import get_num_of_words
+from stats import get_num_of_chars, get_num_of_words
 
 
 def get_book_text(path):
-    with open(path) as file:
+    with open(path, encoding="utf-8-sig") as file:
         return file.read()
 
 
 def main():
     frankenstein_path = "./books/frankenstein.txt"
-    num_words = get_num_of_words(get_book_text(frankenstein_path))
+    text = get_book_text(frankenstein_path)
+    num_words = get_num_of_words(text)
     print(f"Found {num_words} total words")
+    num_chars = get_num_of_chars(text)
+    print(num_chars)
 
 
 main()
--- stats.py
+++ stats.py
@@ -1,2 +1,13 @@
 def get_num_of_words(string):
     return len(string.split())
+
+
+def get_num_of_chars(string):
+    chars = {}
+    for char in string:
+        lower_case = char.lower()
+        if lower_case not in chars:
+            chars[lower_case] = 1
+            continue
+        chars[lower_case] += 1
+    return chars
